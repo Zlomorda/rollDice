0eab50102213389909233c62964266864b0cf758
Zlomorda <61455902+Zlomorda@users.noreply.github.com>
2021-01-03 14:00:21 +0200

    React added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css'
-import RollDice from './RollDice'
+import "./App.css";
+import RollDice from "./RollDice";
+import React from "react";
 
 function App() {
-    return (
-        <div className="App">
-            <RollDice />
-        </div>
-    )
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
 }
 
-export default App
+export default App;
